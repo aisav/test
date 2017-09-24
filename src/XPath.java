0d599ad44f4b569e7b9c8f9d71408b60df88e6e5
arturisav <artur.isaverdyan@gmail.com>
2017-09-24 16:53:04 +0400

    XPath

---
+++ src/XPath.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class XPath {
+
+    public static void main(String[] args) {
+
+        String firefoxdriverpath = "C:\\Users\\user\\IdeaProjects\\selenium1\\lib\\geckodriver.exe";
+        System.setProperty("webdriver.gecko.driver", firefoxdriverpath);
+
+// create a new instance of the Firefox driver
+        WebDriver firefoxDriver = new FirefoxDriver();
+
+// Open the page we want to open
+        firefoxDriver.get("http://www.rediff.com");
+        firefoxDriver.findElement(By.xpath(
+//                "//span[@id='signin_info']/a[1]")).click();
+//                "//span[@id='signin_info']/a[@title='Already a user? Sign in']")).click();
+                "html/body/div[2]/div[4]/span[4]/span/a[@title='Already a user? Sign in']")).click();
+
+
+    }
+}
