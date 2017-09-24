f2aaffd6c1db205b8c51e56d248cb817ab137ca2
arturisav <artur.isaverdyan@gmail.com>
2017-09-24 17:08:40 +0400

    navigate()

--- src/XPath.java
+++ src/XPath.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class XPath {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         String firefoxdriverpath = "C:\\Users\\user\\IdeaProjects\\selenium1\\lib\\geckodriver.exe";
         System.setProperty("webdriver.gecko.driver", firefoxdriverpath);
@@ -19,6 +19,14 @@ public class XPath {
 //                "//span[@id='signin_info']/a[@title='Already a user? Sign in']")).click();
                 "html/body/div[2]/div[4]/span[4]/span/a[@title='Already a user? Sign in']")).click();
 
+        firefoxDriver.navigate().to("http:\\google.com");
+
+        Thread.sleep(4000);
+        firefoxDriver.navigate().back();
+
+        firefoxDriver.navigate().forward();
+
+        firefoxDriver.navigate().refresh();
 
     }
 }
